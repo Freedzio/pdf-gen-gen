06f8dad014b8a19c44d6a519cb3b6ea16cd88fef
Freedzio <luniewski.tomasz@gmail.com>
2022-05-11 17:59:24 +0200

    POC of POC

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import { Button, Col, Container, Row } from 'reactstrap';
+import { useState } from 'react';
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 function App() {
+	const [def, setDef] = useState<TDocumentDefinitions>({
+		footer: undefined,
+		header: undefined,
+		content: [{ text: 'asdasdasd' }]
+	});
+
+	const pdfDocGenerator = pdfMake.createPdf(def);
+
+	pdfDocGenerator.getDataUrl((dataUrl) => {
+		const targetElement = document.querySelector('#iframeContainer');
+		const iframe = document.createElement('iframe');
+		iframe.style.width = '100%';
+		iframe.style.height = '100vh';
+		iframe.src = dataUrl;
+		targetElement?.replaceChildren(iframe);
+	});
+
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<Container>
+			<Row>
+				<Col>wizard</Col>
+				<Col>
+					preview
+					<div id='iframeContainer'></div>
+				</Col>
+			</Row>
+		</Container>
 	);
 }
 
